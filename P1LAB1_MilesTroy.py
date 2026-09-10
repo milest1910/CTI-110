0262d287913c5522e23b0af1b891763c8a65ebc1
milest1910 <milest1910@student.faytechcc.edu>
2026-09-10 11:14:24 -0400

    Add P1LAB1 assignment

---
+++ P1LAB1_MilesTroy.py
@@ -0,0 +1,16 @@
+# Troy Miles Jr
+# 9/10/2026
+# Inputs and Outputs
+
+# Get first name
+first_name = input("Enter your first name: ")
+print()
+
+# Get Last name
+last_name = input("Enter your last name: ")
+print()
+
+# Output welcome message to user
+print(f"Hello, {first_name} {last_name}!, Welcome to CTI-110")
+print()
+print()
